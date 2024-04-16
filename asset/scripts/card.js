0396c7fd0db2676b07fb200fc9a10fddcd7715ad
Your GitHub Username <topsylian@gmail.com>
2024-04-16 14:12:33 +0200

    pushed some new changes

--- asset/scripts/card.js
+++ asset/scripts/card.js
@@ -1,1 +1,94 @@
 "use strict";
+
+//let body = document.querySelector("body.body");
+
+function showPlayer() {
+  let container = document.querySelector("main");
+  let iPlayer = document.createElement("iframe");
+  let optionPlayer = document.createElement("p");
+  let player = document.createElement("a");
+  //let imageBox = document.createElement("div");
+  //let image = document.createElement("img");
+  let movieDisplay = document.createElement("div");
+  let movieImageBox = document.createElement("div");
+  let movieImage = document.createElement("img");
+  let movieDetails = document.createElement("div");
+  let movieTitle = document.createElement("h1");
+  let moviePlot = document.createElement("p");
+  let movieDate = document.createElement("p");
+  let movieDuration = document.createElement("p");
+  let moviePopularity = document.createElement("p");
+  let movieLanguage = document.createElement("p");
+
+  //styling the iFrame to display the video
+  iPlayer.style.width = "100%";
+  iPlayer.style.height = "700px";
+  iPlayer.style.borderRadius = "10px";
+  iPlayer.style.display = "flex";
+  iPlayer.style.background = "red";
+	iPlayer.style.flexDirection = "column";
+	iPlayer.style.margin = "20px 0px";
+  //styling the optionPlayer by clicking another player if the current one failed to load
+  optionPlayer.style.width = "100%";
+  optionPlayer.style.textAlign = "center";
+	optionPlayer.innerText = "If the current player failed, click on other player";
+	optionPlayer.style.marginBottom = "15px";
+  //styling player
+  player.style.width = "200px";
+	player.style.height = "fit-content";
+	player.style.padding = "15px";
+	player.style.background = "rgb(80, 80, 80)";
+	player.style.textAlign = "center";
+	player.style.borderRadius = "10px";
+	player.style.textDecoration = "none";
+	player.style.color = "aliceblue";
+  player.innerText = "Vidsrc";
+  player.setAttribute("href", "http://vidsrc.to/embed/movie");
+  player.addEventListener("click", () => {
+    player.setAttribute("href", "http://vidsrc.to/embed/movie/693134");
+	});
+	player.style.marginBottom = "50px";
+	//styling movieDisplay
+	movieDisplay.style.width = "100%";
+	movieDisplay.style.height = "600px";
+  movieDisplay.style.borderRadius = "10px";
+	movieDisplay.style.display = "flex";
+	movieDisplay.style.gap = "12px";
+  movieDisplay.style.background = "orange";
+	movieDisplay.style.flexDirection = "row";
+	movieDisplay.style.margin = "20px 0px";
+	//styling movieImageBox
+	movieImageBox.style.width = "20%";
+	movieImageBox.style.height = "100%";
+  movieImageBox.style.borderRadius = "10px";
+	movieImageBox.style.display = "flex";
+  movieImageBox.style.background = "teal";
+	//styling movieImage
+	movieImage.style.width = "100%";
+	movieImage.style.height = "100%";
+	movieImage.style.borderRadius = "10px";
+	//styling movieDetails
+	movieDetails.style.width = "80%";
+  movieDetails.style.height = "100%";
+  movieDetails.style.borderRadius = "10px";
+  movieDetails.style.display = "flex";
+  movieDetails.style.background = "red";
+	movieDetails.style.flexDirection = "column";
+	//styling movieTitle
+
+  container.appendChild(iPlayer);
+  container.appendChild(optionPlayer);
+	container.appendChild(player);
+	container.appendChild(movieDisplay);
+	movieDisplay.appendChild(movieImageBox);
+	movieDisplay.appendChild(movieDetails);
+	movieImageBox.appendChild(movieImage);
+	movieDetails.appendChild(movieTitle);
+	movieDetails.appendChild(moviePlot);
+	movieDetails.appendChild(movieDate);
+	movieDetails.appendChild(movieDuration);
+	movieDetails.appendChild(moviePopularity);
+	movieDetails.appendChild(movieLanguage);
+}
+
+showPlayer();
--- asset/scripts/dashboard.js
+++ asset/scripts/dashboard.js
@@ -1,5 +1,7 @@
 "use strict";
+
 /*The code below is to fetch the api data in an asynchronous way*/
+//import { showPlayer } from "./card.js";
 
 async function fetchMovies() {
   try {
@@ -16,6 +18,21 @@ async function fetchMovies() {
   }
 }
 
+async function fetchPopular() {
+  try {
+    const response = await fetch("https://movieapp-zyqr.onrender.com/api/v1/popular");
+    const data = await response.json();
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.log(
+      `Getting data from api resulted in an error and ${error.message}`
+    ); 
+  }
+}
+
+const popularPromise = fetchPopular();
+
 //saving the return Promise into a variable called promise to make use of the return data.
 
 let promise = fetchMovies();
@@ -23,23 +40,27 @@ let promise = fetchMovies();
 let mainMovieTitle = document.querySelector(".title > h1");
 let mainImage = document.querySelector(".mainCard > img");
 let mainDate = document.querySelector(".movie-year");
-let mainRating = document.querySelector(".movie-rate");
+//let mainRating = document.querySelector(".movie-rate");
 let mainPlot = document.querySelector(".movie-plot > p");
 let mainPopularity = document.querySelector(".popularity");
 
 //Declare variable that display other movie title, genre, runtime, votes, and language.
 
 let nowPlayingMovie = document.querySelector(".now-playing-movie-box");
-let popularMovie = document.querySelector(".popular-movie");
+let popularMovie = document.querySelector(".popular-movie-box");
+
+let frameLen = 20; //number of videos in the home page;
 
 promise
   .then(function (data) {
     let dataLen = data.length;
-    let frameLen = 21; //number of videos in the home page;
+
     console.log(data[0]);
     //Using the first data from the promise object for the mainFrame by showcasing the images from the api.
+
     //setting the default image to display;
     mainImage.src = data[0].profile_picture_url;
+    console.log(data[0]);
     //setting the movie title to the main movie frame
     mainMovieTitle.innerText = data[0].original_title;
     //setting the movie story to the main movie frame
@@ -49,7 +70,8 @@ promise
     //setting the movie date to the main movie frame
     mainDate.innerText = `Release Date: ${data[0].release_date}`;
     //create element for each movie object from the Api data
-    for (let j = 0; j < 20; j++) {
+
+    for (let j = 0; j < frameLen; j++) {
       function nowPlayingMoviesList() {
         let movies = document.createElement("div");
         let movieContent = document.createElement("div");
@@ -59,12 +81,12 @@ promise
         let movieTitle = document.createElement("h4");
         let movieDate = document.createElement("p");
         let moviePopularity = document.createElement("p");
+        let watch = document.createElement("a");
         //styling movie box
         movies.style.width = "23%";
         movies.style.height = "400px";
         movies.style.borderRadius = "10px";
         movies.style.background = "rgb(80, 80, 80)";
-        movies.setAttribute("href", "./page/card.html");
         movies.style.textDecoration = "none";
         //styling movie content box
         movieContent.style.width = "100%";
@@ -76,14 +98,15 @@ promise
         movieImageBox.style.width = "100%";
         movieImageBox.style.height = "300px";
         movieImageBox.style.background = "rgb(33, 33, 33, 0.5)";
-        movieImageBox.setAttribute("href", `${data[j].profile_picture_url}`);
-        movieImageBox.style.textDecoration = "none";
+        movieImageBox.style.display = "flex";
+        movieImageBox.setAttribute("href", `./page/card.html`);
+        //movieImageBox.style.textDecoration = "none";
         //styling movie image
         movieImage.style.width = "100%";
         movieImage.style.height = "300px";
         movieImage.style.objectFit = "cover";
         movieImage.style.borderRadius = "10px 10px 0px 0px";
-        movieImage.src = data[j].profile_picture_url;
+        movieImage.setAttribute("src", data[j].profile_picture_url);
         //styling movie details
         movieDetails.style.width = "100%";
         movieDetails.style.background = "rgb(33, 33, 33, 0.5)";
@@ -102,7 +125,6 @@ promise
         //styling movie title
         moviePopularity.style.width = "100%";
         moviePopularity.innerHTML = `Popularity: ${data[j].popularity}`;
-    
         //Appending the elements to make each movie frame
         nowPlayingMovie.appendChild(movies);
         movies.appendChild(movieContent);
@@ -113,7 +135,7 @@ promise
         movieDetails.appendChild(movieDate);
         movieDetails.appendChild(moviePopularity);
       }
-      
+
       nowPlayingMoviesList();
     }
   })
@@ -121,7 +143,99 @@ promise
     console.log(`The error is ${err}`);
   });
 
-"https://moviesfrees.netlify.app/movie_overview/693134"
+popularPromise
+  .then((data) => {
+    for (let j = 0; j < frameLen; j++) {
+      function popularMoviesList() {
+        let movies = document.createElement("div");
+        let movieContent = document.createElement("div");
+        let movieImageBox = document.createElement("a");
+        let movieImage = document.createElement("img");
+        let movieDetails = document.createElement("div");
+        let movieTitle = document.createElement("h4");
+        let movieDate = document.createElement("p");
+        let moviePopularity = document.createElement("p");
+        let watch = document.createElement("a");
+        //styling movie box
+        movies.style.width = "23%";
+        movies.style.height = "400px";
+        movies.style.borderRadius = "10px";
+        movies.style.background = "rgb(80, 80, 80)";
+        movies.style.textDecoration = "none";
+        //styling movie content box
+        movieContent.style.width = "100%";
+        movieContent.style.height = "100%";
+        movieContent.style.borderRadius = "10px";
+        movieContent.style.display = "flex";
+        movieContent.style.flexDirection = "column";
+        //styling movie image box
+        movieImageBox.style.width = "100%";
+        movieImageBox.style.height = "300px";
+        movieImageBox.style.background = "rgb(33, 33, 33, 0.5)";
+        movieImageBox.style.display = "flex";
+        movieImageBox.setAttribute("href", `./page/card.html`);
+        //movieImageBox.style.textDecoration = "none";
+        //styling movie image
+        movieImage.style.width = "100%";
+        movieImage.style.height = "300px";
+        movieImage.style.objectFit = "cover";
+        movieImage.style.borderRadius = "10px 10px 0px 0px";
+        movieImage.setAttribute("src", data[j].profile_picture_url);
+        //styling movie details
+        movieDetails.style.width = "100%";
+        movieDetails.style.background = "rgb(33, 33, 33, 0.5)";
+        movieDetails.style.height = "100%";
+        movieDetails.style.borderRadius = "0px 0px 10px 10px";
+        movieDetails.style.padding = "12px";
+        movieDetails.style.display = "flex";
+        movieDetails.style.flexDirection = "column";
+        movieDetails.style.gap = "5px";
+        //styling movie title
+        movieTitle.style.width = "100%";
+        movieTitle.innerHTML = data[j].original_title;
+        //styling movie title
+        movieDate.style.width = "100%";
+        movieDate.innerHTML = `Release Date: ${data[j].release_date}`;
+        //styling movie title
+        moviePopularity.style.width = "100%";
+        moviePopularity.innerHTML = `Popularity: ${data[j].popularity}`;
+        //Appending the elements to make each movie frame
+        popularMovie.appendChild(movies);
+        movies.appendChild(movieContent);
+        movieContent.appendChild(movieImageBox);
+        movieImageBox.appendChild(movieImage);
+        movieContent.appendChild(movieDetails);
+        movieDetails.appendChild(movieTitle);
+        movieDetails.appendChild(movieDate);
+        movieDetails.appendChild(moviePopularity);
+      }
+  
+      popularMoviesList();
+    }
+  })
+  .catch((err) => {
+    console.log(err)
+  });
+
+//("https://moviesfrees.netlify.app/movie_overview/693134");
+
+//styling the watch
+/*
+        watch.style.width = "100%";
+        watch.style.display = "flex";
+        watch.style.color = "aliceblue";
+        watch.setAttribute("href", `/page/card.html`);
+        watch.style.textDecoration = "none";
+        watch.innerText = "Watch";
+        watch.addEventListener("click", async () => {
+          try {
+            let showPlayer = await import('./card.js');
+            showPlayer();
+          } catch (error) {
+            console.log(error);
+          }
+        });
+*/
 
 let homeBtn = document.querySelector("a.home-link");
 const menuButtons = document.querySelectorAll(".menu-box > section > a");
@@ -191,10 +305,3 @@ for (let i = 0; i < menuButtons.length; i++) {
 }
 
 //The code for the dashboard menu-icons ends here 🤒🤒
-
-/*The code for the coming soon button that changes the innerText to a marked text that a movie is successfully added to wishlist*/
-
-//select all remind me buttons using querySelector
-const remindBtn = document.querySelectorAll(".remind-me-later");
-
-//select all P and span that hold the 🔔 emoji inside of the remind me buttons using querySelector
